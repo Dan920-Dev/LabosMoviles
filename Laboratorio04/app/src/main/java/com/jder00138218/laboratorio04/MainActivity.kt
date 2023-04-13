787f89e4168863a1e6b09d0291c084bef1261a59
Dan920-Dev <00138218@uca.edu.sv>
2023-04-13 09:21:57 -0600

    add navigation and fix views

--- Laboratorio04/app/src/main/java/com/jder00138218/laboratorio04/MainActivity.kt
+++ Laboratorio04/app/src/main/java/com/jder00138218/laboratorio04/MainActivity.kt
@@ -4,19 +4,57 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var saveButton: Button
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var phoneEditText: EditText
+
+    // Data
+    private var name = ""
+    private var phone = 0
+    private var email = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bind()
 
+        saveButton.setOnClickListener {
+            onSend()
+        }
 
     }
 
+    private  fun bind(){
+        saveButton = findViewById(R.id.save_button)
+        nameEditText = findViewById(R.id.name_editText)
+        phoneEditText = findViewById(R.id.phone_editText)
+        emailEditText = findViewById(R.id.email_editText)
+    }
 
+    private fun onSend(){
+        // Data
+        name = nameEditText.text.toString()
+        email = emailEditText.text.toString()
+        phone = phoneEditText.text.toString().toInt()
 
+        // Intent
+        val intent = Intent(this, ShareActivity::class.java)
+        intent.putExtra(KEY_NAME_USER, name)
+        intent.putExtra(KEY_EMAIL_USER, email)
+        intent.putExtra(KEY_PHONE_USER, phone)
+        startActivity(intent)
+    }
+
+    companion object{
+        const val KEY_NAME_USER = "Name"
+        const val KEY_PHONE_USER = "phone"
+        const val KEY_EMAIL_USER = "Email"
+    }
 
 }
--- Laboratorio04/app/src/main/java/com/jder00138218/laboratorio04/ShareActivity.kt
+++ Laboratorio04/app/src/main/java/com/jder00138218/laboratorio04/ShareActivity.kt
@@ -2,10 +2,49 @@ package com.jder00138218.laboratorio04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class ShareActivity : AppCompatActivity() {
+
+    // Data
+    private var name = ""
+    private var phone = 0
+    private var email = ""
+
+    // Views
+    private lateinit var shareButton: Button
+    private lateinit var nameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var phoneTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share)
+
+        bind()
+
+        intent?.let { intent ->
+            name = (intent.getStringExtra(MainActivity.KEY_NAME_USER)).toString()
+            email = (intent.getStringExtra(MainActivity.KEY_EMAIL_USER)).toString()
+            phone = intent.getIntExtra(MainActivity.KEY_PHONE_USER, 0)
+        }
+
+        information(name, email, phone)
+
+    }
+
+    private fun bind(){
+        shareButton = findViewById(R.id.share_button)
+        nameTextView = findViewById(R.id.name_share__textView)
+        phoneTextView = findViewById(R.id.phone_share_textView)
+        emailTextView = findViewById(R.id.email_share_textView)
+    }
+
+    fun information(name: String, email: String, phone: Int){
+        nameTextView.text = name
+        phoneTextView.text = phone.toString()
+        emailTextView.text = email
     }
 }
